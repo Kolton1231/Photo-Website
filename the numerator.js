7f5708159407ef6dc6d6375edc3129bc3003d9bf
Kolton1231 <koltonjames1231@gmail.com>
2026-04-04 00:02:27 -0400

    INCOMPLETE CORTLAND
    Incomplete, added new page and images, still have to sort.

---
+++ the numerator.js
@@ -0,0 +1,30 @@
+var integer = 0;
+var largeImageLoc = "";
+var compImageLoc = "";
+
+
+const fs = require('fs');
+
+const directoryPath = 'C:/replace/these/with/directory'; // Change this to your directory path
+const files = fs.readdirSync(directoryPath);
+
+
+for (var i = 0; i < files.length; i++){
+    compImageLoc = files[i];
+    largeImageLoc = files[i];
+    console.log("<a href=\"Images/" + largeImageLoc + "\"> <img src=\"imagesSmall/" + compImageLoc + "\" class=\"imagepadding\"> </a>");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//<a href="Images/img57.jpg"> <img src="imagesSmall/img57.jpg" class="imagepadding"> </a>
